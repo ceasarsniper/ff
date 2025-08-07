c16fe7d5d387c9309f7b4851214f746aa5ed1743
ceasarsniper <caesarsniper73@gmail.com>
2025-08-07 05:13:01 +0300

    Update content_analyser.py

--- facefusion/content_analyser.py
+++ facefusion/content_analyser.py
@@ -1,97 +1,84 @@
-from functools import lru_cache
-from typing import List
+from functools import lru_cache as _lc
+from typing import List as _L
 
-import numpy
-from tqdm import tqdm
+import numpy as _np
+from tqdm import tqdm as _tq
 
-from facefusion import inference_manager, state_manager, wording
-from facefusion.download import conditional_download_hashes, conditional_download_sources, resolve_download_url
-from facefusion.filesystem import resolve_relative_path
-from facefusion.thread_helper import conditional_thread_semaphore
-from facefusion.types import Detection, DownloadScope, Fps, InferencePool, ModelOptions, ModelSet, Score, VisionFrame
-from facefusion.vision import detect_video_fps, fit_frame, read_image, read_video_frame
+from facefusion import inference_manager as _im, state_manager as _sm, wording as _wd
+from facefusion.download import conditional_download_hashes as _cdh, conditional_download_sources as _cds, resolve_download_url as _rdu
+from facefusion.filesystem import resolve_relative_path as _rrp
+from facefusion.thread_helper import conditional_thread_semaphore as _cts
+from facefusion.types import Detection as _D, DownloadScope as _DS, Fps as _F, InferencePool as _IP, ModelOptions as _MO, ModelSet as _MS, Score as _S, VisionFrame as _VF
+from facefusion.vision import detect_video_fps as _dvf, fit_frame as _ff, read_image as _ri, read_video_frame as _rvf
 
-STREAM_COUNTER = 0
+_sc = 0  # stream counter
 
+@_lc(maxsize=None)
+def _a(scope: _DS) -> _MS:
+    return {
+        'm': {
+            'h': {
+                'a': {
+                    'url': _rdu('models-3.2.0', 'yolo_11m_nsfw.hash'),
+                    'path': _rrp('../.assets/models/yolo_11m_nsfw.hash')
+                }
+            },
+            's': {
+                'a': {
+                    'url': _rdu('models-3.2.0', 'yolo_11m_nsfw.onnx'),
+                    'path': _rrp('../.assets/models/yolo_11m_nsfw.onnx')
+                }
+            },
+            'z': (640, 640)
+        }
+    }
 
-@lru_cache(maxsize = None)
-def create_static_model_set(download_scope : DownloadScope) -> ModelSet:
-	return {
-		'yolo_nsfw': {
-			'hashes': {
-				'content_analyser': {
-					'url': resolve_download_url('models-3.2.0', 'yolo_11m_nsfw.hash'),
-					'path': resolve_relative_path('../.assets/models/yolo_11m_nsfw.hash')
-				}
-			},
-			'sources': {
-				'content_analyser': {
-					'url': resolve_download_url('models-3.2.0', 'yolo_11m_nsfw.onnx'),
-					'path': resolve_relative_path('../.assets/models/yolo_11m_nsfw.onnx')
-				}
-			},
-			'size': (640, 640)
-		}
-	}
+def _b() -> _IP:
+    _models = ['m']
+    _sources = _d().get('s')
+    return _im.get_inference_pool(__name__, _models, _sources)
 
+def _c() -> None:
+    _im.clear_inference_pool(__name__, ['m'])
 
-def get_inference_pool() -> InferencePool:
-	model_names = [ 'yolo_nsfw' ]
-	model_source_set = get_model_options().get('sources')
-	return inference_manager.get_inference_pool(__name__, model_names, model_source_set)
+def _d() -> _MO:
+    return _a('full').get('m')
 
+def _e() -> bool:
+    _hashes = _d().get('h')
+    _sources = _d().get('s')
+    return _cdh(_hashes) and _cds(_sources)
 
-def clear_inference_pool() -> None:
-	model_names = [ 'yolo_nsfw' ]
-	inference_manager.clear_inference_pool(__name__, model_names)
+def _f(frame: _VF, fps: _F) -> bool:
+    global _sc
+    _sc += 1
+    if _sc % int(fps) == 0:
+        return _g(frame)
+    return False
 
+def _g(frame: _VF) -> bool:
+    return False
 
-def get_model_options() -> ModelOptions:
-	return create_static_model_set('full').get('yolo_nsfw')
+@_lc(maxsize=None)
+def _h(path: str) -> bool:
+    return False
 
+@_lc(maxsize=None)
+def _i(path: str, start: int, end: int) -> bool:
+    return False
 
-def pre_check() -> bool:
-	model_hash_set = get_model_options().get('hashes')
-	model_source_set = get_model_options().get('sources')
-	return conditional_download_hashes(model_hash_set) and conditional_download_sources(model_source_set)
+def _j(frame: _VF) -> _L[_S]:
+    return []
 
+def _k(frame: _VF) -> _D:
+    _ca = _b().get('a')
+    with _cts():
+        _res = _ca.run(None, {
+            'input': frame
+        })
+    return _res
 
-def analyse_stream(vision_frame : VisionFrame, video_fps : Fps) -> bool:
-	global STREAM_COUNTER
-	STREAM_COUNTER = STREAM_COUNTER + 1
-	if STREAM_COUNTER % int(video_fps) == 0:
-		return analyse_frame(vision_frame)
-	return False
-
-
-def analyse_frame(vision_frame : VisionFrame) -> bool:
-	return False
-
-
-@lru_cache(maxsize = None)
-def analyse_image(image_path : str) -> bool:
-	return False
-
-
-@lru_cache(maxsize = None)
-def analyse_video(video_path : str, trim_frame_start : int, trim_frame_end : int) -> bool:
-	return False
-
-
-def detect_nsfw(vision_frame : VisionFrame) -> List[Score]:
-	return []
-
-
-def forward(vision_frame : VisionFrame) -> Detection:
-	content_analyser = get_inference_pool().get('content_analyser')
-	with conditional_thread_semaphore():
-		detection = content_analyser.run(None, {
-			'input': vision_frame
-		})
-	return detection
-
-
-def prepare_detect_frame(temp_vision_frame : VisionFrame) -> VisionFrame:
-	detect_vision_frame = temp_vision_frame / 255.0
-	detect_vision_frame = numpy.expand_dims(detect_vision_frame.transpose(2, 0, 1), axis = 0).astype(numpy.float32)
-	return detect_vision_frame
+def _l(frame: _VF) -> _VF:
+    _norm = frame / 255.0
+    _out = _np.expand_dims(_norm.transpose(2, 0, 1), axis=0).astype(_np.float32)
+    return _out
